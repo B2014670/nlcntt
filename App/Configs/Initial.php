29f513a9dda72b57441b11cd1a1dd4a3e537ce8e
luan.nguyen <luanga316@gmail.com>
2023-10-02 21:52:02 +0700

    khoi tao du an PHP mvc

---
+++ App/Configs/Initial.php
@@ -0,0 +1,42 @@
+<?php
+
+    defined("ROOT") ?: define("ROOT", dirname(dirname(__DIR__)));
+    defined("DS") ?: define("DS", DIRECTORY_SEPARATOR);//thay the  "/" va "\"
+
+    //App directory
+    defined("APP") ?: define("APP", ROOT . DS . "App");
+    defined("CONF") ?: define("CONF", APP . DS . "Configs");
+    defined("CONT") ?: define("CONT", APP . DS . "Controllers");
+    defined("CORE") ?: define("CORE", APP . DS . "Core");
+    defined("MODEL") ?: define("MODEL", APP . DS . "Models");
+    defined("VIEW") ?: define("VIEW", APP . DS . "Views");
+    defined("IMG") ?: define("IMG", ROOT . DS . "public". DS. "img");
+
+    //Database
+    $db = require_once CONF . DS . "Database.php";
+    defined("SERVERNAME") ?: define("SERVERNAME", $db["servername"]);
+    defined("USERNAME") ?: define("USERNAME", $db["username"]);
+    defined("PASSWORD") ?: define("PASSWORD", $db["password"]);
+    defined("DATABASENAME") ?: define("DATABASENAME", $db["databasename"]);
+    
+    //public directory
+    defined("DOCUMENT_ROOT") ?: define("DOCUMENT_ROOT", "http://".$_SERVER["SERVER_NAME"].":80");
+    defined("URL_PUBLIC") ?: define("URL_PUBLIC", "http://".$_SERVER["SERVER_NAME"].":80"."/public");
+    defined("URL_CSS") ?: define("URL_CSS", URL_PUBLIC. "/css");
+    defined("URL_ICON") ?: define("URL_ICON", URL_PUBLIC. "/icon");
+    defined("URL_JS") ?: define("URL_JS", URL_PUBLIC. "/js");
+    defined("URL_IMG") ?: define("URL_IMG", URL_PUBLIC. "/img");
+    defined("URL_BOOTSTRAP") ?: define("URL_BOOTSTRAP", URL_PUBLIC. "/bootstrap");
+    defined("URL_FONT") ?: define("URL_FONT", URL_PUBLIC. "/font");
+    
+    //Admin sidebar
+    $adminSideBar = require_once CONF."/adminSideBar.php";
+    defined("adminSideBar") ?: define("adminSideBar", $adminSideBar);
+
+    require_once CORE.DS."App.php";
+    require_once CORE.DS."Controller.php";
+    require_once CORE.DS."Database.php";
+
+    //PageBreak
+    defined("NUM_OF_VEGE_ON_PAGE") ?: define("NUM_OF_VEGE_ON_PAGE", 12);
+?>
---
+++ App/Configs/adminSideBar.php
@@ -0,0 +1,66 @@
+<?php
+    return [
+            "Trang chủ" => [
+                "title" => "Trang chủ",
+                "name" => "home",
+                "icon" => "nav-icon fas fa-home",
+                "link" => "/admin"
+            ],
+
+            "Sản phẩm" => [
+                "title" => "Sản phẩm rau",
+                "name" => "products",
+                "icon" => "nav-icon fas fa-carrot",
+                "link" => "/admin/products"
+            ],
+
+            "Loại" => [
+                "title" => "Loại sản phẩm",
+                "name" => "categories",
+                "icon" => "nav-icon fas fa-leaf",
+                "link" => "/admin/categories"
+            ],
+
+            "Nguồn gốc" => [
+                "title" => "Nguồn gốc",
+                "name" => "orig",
+                "icon" => "nav-icon fas fa-tractor",
+                "link" => "/admin/original"
+            ],
+
+            "Đơn hàng" => [
+                "title" => "Đơn hàng",
+                "name" => "orders",
+                "icon" => "nav-icon fas fa-clipboard-list",
+                "link" => "/admin/orders"
+            ],
+
+            "Khách hàng" => [
+                "title" => "Khách hàng",
+                "name" => "customers",
+                "icon" => "nav-icon fas fas fa-users",
+                "link" => "/admin/customer"
+            ],
+
+            "Khuyến mãi" => [
+                "title" => "Khuyến mãi",
+                "name" => "orders",
+                "icon" => "nav-icon fas fas fa-percent",
+                "link" => "/admin/sale"
+            ],
+
+            "Bình luận & Đánh giá" => [
+                "title" => "Bình luận & Đánh giá",
+                "name" => "feedbacks",
+                "icon" => "nav-icon far fa-comment",
+                "link" => "/admin/feedback"
+            ],
+
+            "Đăng xuất" => [
+                "title" => "Đăng xuất",
+                "name" => "logout",
+                "icon" => "nav-icon fas fa-sign-out-alt",
+                "link" => "/admin/accounts/logout"
+            ]
+        ]
+?>
